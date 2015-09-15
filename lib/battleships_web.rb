a5b0ef138510258287e1db815890174a75a8a500
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-15 11:35:15 +0200

    first feature test pass

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
+  set :views, proc {File.join(root, '..', 'views')}
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  get '/new_game.erb' do
+  erb :new_game
   end
 
   # start the server if ruby file executed directly
